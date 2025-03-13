1c8cc29a9c74239f9e353ce8ee4e64b4b33b8a6b
K-Sym <keith@tamedmedia.com>
2025-03-13 21:40:10 +0000

    Set the visualiser as the default view

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,16 +3,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'visualizer', component: () => import('pages/VisualizerPage.vue') }
+      { path: '', component: () => import('pages/VisualizerPage.vue') }
     ]
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
   }
 ]
 
